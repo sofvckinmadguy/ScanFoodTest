3f30208b842e28974181e5443635158a9d7647fe
sofvckinmadguy <segalzakhar1999@mail.ru>
2021-11-29 18:53:08 +0300

    added model

--- ScanFoodTest/ViewController.swift
+++ ScanFoodTest/ViewController.swift
@@ -6,14 +6,53 @@
 //
 
 import UIKit
+import AVKit
+import Vision
 
 class ViewController: UIViewController {
+    
+    var captureSession: AVCaptureSession = AVCaptureSession()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupCaptureSession()
+        setupPreviewLayer()
+        
+        let dataOutput = AVCaptureVideoDataOutput()
+        dataOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "videoQueue"))
+        captureSession.addOutput(dataOutput)
     }
 
 
+    func setupCaptureSession() {
+        captureSession.sessionPreset = .photo
+        
+        guard let captureDevice = AVCaptureDevice.default(for: .video) else { return }
+        
+        guard let input = try? AVCaptureDeviceInput(device: captureDevice) else { return }
+        
+        captureSession.addInput(input)
+        
+        captureSession.startRunning()
+    }
+    
+    func setupPreviewLayer() {
+        let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        
+        view.layer.addSublayer(previewLayer)
+        
+        previewLayer.frame = view.frame
+    }
 }
 
+extension ViewController: AVCaptureVideoDataOutputSampleBufferDelegate {
+    
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        guard let pixelBuffer: CVPixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else { return }
+        guard let model = try? VNCoreMLModel(for: SqueezeNet().model) else { return }
+        let request = VNCoreMLRequest(model: model) { finishRequest, error in
+            print(finishRequest.results)
+        }
+        try? VNImageRequestHandler(cvPixelBuffer: pixelBuffer, options: [:]).perform([request])
+    }
+}
